bfcdddcfbe9877f0b69fcfc8dd645492651d763d
LostEugen <155205442+LostEugen@users.noreply.github.com>
2024-01-04 02:07:13 +0100

    button funktioniert ein bisschen besser (hoffe ich)

--- pixart rpg/Assets/settings_button.cs
+++ pixart rpg/Assets/settings_button.cs
@@ -1,14 +1,8 @@
-
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class settings_button : MonoBehaviour {
-    public void GoToScene(string sceneName) { 
-    SceneManager.LoadScene(sceneName);
-    }    
 
-    public void QuitApp() {
-        Application.Quit();
-        Debug.Log("Application has quit.");
-    }
-}
+   
+    
